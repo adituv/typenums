bf2d654c64fba4455c7deb6a0d9e39c6bed356c1
Iris Ward <aditu.venyhandottir@gmail.com>
2021-03-19 20:44:00 +0000

    Draft simplifying rationals

--- src/Data/TypeNums/Arithmetic/Internal.hs
+++ src/Data/TypeNums/Arithmetic/Internal.hs
@@ -15,12 +15,16 @@
 
 -- | This module exposes the inner workings of type-level arithmetic for
 --   further extensions.
-module Data.TypeNums.Arithmetic.Internal where
+module Data.TypeNums.Arithmetic.Internal (
+  AddK, SubK, MulK, IntDivK, Abs, Negate, Recip, Simplify, Add, Sub, Mul, Div, DivMod, QuotRem, IntDiv, Mod, Quot, Rem, GCD
+) where
 
 import Data.Type.Bool     (If)
+import Data.TypeNums.Equality (type (==?))
 import Data.TypeNums.Ints
 import Data.TypeNums.Rats
-import GHC.TypeLits
+import GHC.TypeLits(Nat)
+import qualified GHC.TypeLits as G
 
 -- TODO: Reduce type-level rationals?  Probably not 100% necessary as the
 -- user should only see them via ratVal which already handles reduction
@@ -75,29 +79,37 @@ type family IntDivK k where
   IntDivK Nat = Nat
   IntDivK TInt = TInt
 
+-- | The kind of the result of finding the GCD of two integers
+--
+-- @since 0.1.4
+type family GCDK k1 k2 where
+  GCDK Nat Nat = Nat
+  GCDK TInt _ = TInt
+  GCDK _ TInt = TInt
+
 -- | The sum of two type-level numbers.
 --
 -- @since 0.1.2
 type family Add (x :: k1) (y :: k2) :: AddK k1 k2 where
-  Add (x :: Nat) (y :: Nat)  = x + y
+  Add (x :: Nat) (y :: Nat)  = x G.+ y
 
-  Add ('Pos x)   ('Pos y)   = 'Pos (x + y)
-  Add ('Neg x)   ('Pos y)   = If (x <=? y)
-                                            ('Pos (y - x))
-                                            ('Neg (x - y))
-  Add ('Pos x)   ('Neg y)   = If (y <=? x)
-                                            ('Pos (x - y))
-                                            ('Neg (y - x))
-  Add ('Neg x)   ('Neg y)   = 'Neg (x + y)
+  Add ('Pos x)   ('Pos y)   = 'Pos (x G.+ y)
+  Add ('Neg x)   ('Pos y)   = If (x G.<=? y)
+                                            ('Pos (y G.- x))
+                                            ('Neg (x G.- y))
+  Add ('Pos x)   ('Neg y)   = If (y G.<=? x)
+                                            ('Pos (x G.- y))
+                                            ('Neg (y G.- x))
+  Add ('Neg x)   ('Neg y)   = 'Neg (x G.+ y)
 
-  Add ('Pos x)   (y :: Nat) = 'Pos (x + y)
-  Add ('Neg x)   (y :: Nat) = If (x <=? y)
-                                            ('Pos (y - x))
-                                            ('Neg (x - y))
-  Add (x :: Nat)  ('Pos y)  = 'Pos (x + y)
-  Add (x :: Nat)  ('Neg y)  = If (y <=? x)
-                                            ('Pos (x - y))
-                                            ('Neg (y - x))
+  Add ('Pos x)   (y :: Nat) = 'Pos (x G.+ y)
+  Add ('Neg x)   (y :: Nat) = If (x G.<=? y)
+                                            ('Pos (y G.- x))
+                                            ('Neg (x G.- y))
+  Add (x :: Nat)  ('Pos y)  = 'Pos (x G.+ y)
+  Add (x :: Nat)  ('Neg y)  = If (y G.<=? x)
+                                            ('Pos (x G.- y))
+                                            ('Neg (y G.- x))
 
   Add (n1 ':% d1) (n2 ':% d2) = (Add (Mul n1 d2) (Mul n2 d1))
                                             ':% (Mul d1 d2)
@@ -112,7 +124,7 @@ type family Add (x :: k1) (y :: k2) :: AddK k1 k2 where
 --
 -- @since 0.1.2
 type family Sub (x :: k1) (y :: k2) :: SubK k1 k2 where
-  Sub (x :: Nat) (y :: Nat)  = x - y
+  Sub (x :: Nat) (y :: Nat)  = x G.- y
 
   Sub ('Pos x)   ('Pos y)   = Add x ('Neg y)
   Sub ('Neg x)   ('Pos y)   = Add ('Neg x) ('Neg y)
@@ -140,17 +152,17 @@ type family Sub (x :: k1) (y :: k2) :: SubK k1 k2 where
 --
 -- @since 0.1.2
 type family Mul (x :: k1) (y :: k2) :: MulK k1 k2 where
-  Mul (x :: Nat) (y :: Nat) = x * y
+  Mul (x :: Nat) (y :: Nat) = x G.* y
 
-  Mul ('Pos x)   ('Pos y)   = 'Pos (x * y)
-  Mul ('Neg x)   ('Pos y)   = 'Neg (x * y)
-  Mul ('Pos x)   ('Neg y)   = 'Neg (x * y)
-  Mul ('Neg x)   ('Neg y)   = 'Pos (x * y)
+  Mul ('Pos x)   ('Pos y)   = 'Pos (x G.* y)
+  Mul ('Neg x)   ('Pos y)   = 'Neg (x G.* y)
+  Mul ('Pos x)   ('Neg y)   = 'Neg (x G.* y)
+  Mul ('Neg x)   ('Neg y)   = 'Pos (x G.* y)
 
-  Mul ('Pos x)   (y :: Nat) = 'Pos (x * y)
-  Mul ('Neg x)   (y :: Nat) = 'Neg (x * y)
-  Mul (x :: Nat)  ('Pos y)  = 'Pos (x * y)
-  Mul (x :: Nat)  ('Neg y)  = 'Neg (x * y)
+  Mul ('Pos x)   (y :: Nat) = 'Pos (x G.* y)
+  Mul ('Neg x)   (y :: Nat) = 'Neg (x G.* y)
+  Mul (x :: Nat)  ('Pos y)  = 'Pos (x G.* y)
+  Mul (x :: Nat)  ('Neg y)  = 'Neg (x G.* y)
 
   Mul (n1 ':% d1) (n2 ':% d2) = (Mul n1 n2) ':% (Mul d1 d2)
   Mul (n ':% d)   (y :: Nat)  = (Mul n y) ':% d
@@ -159,6 +171,27 @@ type family Mul (x :: k1) (y :: k2) :: MulK k1 k2 where
   Mul (x :: Nat)  (n ':% d)   = (Mul x n) ':% d
   Mul ('Pos x)    (n ':% d)   = (Mul ('Pos x) n) ':% d
   Mul ('Neg x)    (n ':% d)   = (Mul ('Neg x) n) ':% d
+
+-- | The reciprocal of a type-level rational
+--
+-- @since 0.1.4
+type family Recip (x :: Rat) :: Rat where
+  Recip (('Pos x) ':% y) = ('Pos y) ':% x
+  Recip (('Neg x) ':% y) = ('Neg y) ':% x
+  Recip ((x :: Nat) ':% y) = y ':% x
+
+-- | The result of dividing two type-level numbers.
+--
+-- @since 0.1.4
+type family Div (x :: k1) (y :: k2) :: Rat where
+  Div (x :: Nat) (y :: Nat) = x ':% y
+  Div (x :: TInt) (y :: Nat) = x ':% y
+  Div (x :: Nat) ('Neg y) = ('Neg x) ':% y
+  Div (x :: TInt) ('Neg y) = (Negate x) ':% y
+  Div (x :: TInt) ('Pos y) = x ':% y
+  Div (x :: Rat) (y :: Rat) = Mul x (Recip y)
+  Div x (y :: Rat) = Mul (Div x 1) (Recip y)
+  Div (x :: Rat) y = Mul (Recip x) (Div y 1)
 
 -- | The result of negating a 'TInt'
 -- 
@@ -169,27 +202,97 @@ type family Negate (x :: TInt) :: TInt where
   Negate ('Pos x) = 'Neg x
   Negate ('Neg x) = 'Pos x
 
--- | The quotient and remainder of a type-level integer and a natural number
---
+-- | The quotient and remainder of a type-level integer and a natural number.
+-- 
 -- @since 0.1.4
 type family DivMod (x :: k) (y :: Nat) :: (IntDivK k, IntDivK k) where
-  DivMod _ 0 = TypeError ('Text "Divisor must not be 0")
+  DivMod _ 0 = G.TypeError ('G.Text "Divisor must not be 0")
   DivMod (x :: Nat) y = UnPos (DivModAux ('Pos x) y ('Pos 0))
   DivMod ('Pos x) y = DivModAux ('Pos x) y ('Pos 0)
   DivMod ('Neg x) y = DivModNegFixup (DivModAux ('Pos x) y ('Pos 0))
 
--- DivModAux for a negative x calculates towards zero instead of down,
--- so it is off by 1.  This handles the off-by-one error and the
--- required negation of the quotient
+-- |
+--
+-- @since 0.1.4
+type family QuotRem (x :: k) (y :: Nat) :: (IntDivK k, IntDivK k) where
+  QuotRem ('Neg x) y = QuotRemFixup (DivMod ('Neg x) y) y
+  QuotRem x y = DivMod x y
+
+-- | The quotient of a type-level integer and a natural number.
+--   
+-- @since 0.1.4
+type family IntDiv (x :: k) (y :: Nat) :: IntDivK k where
+  IntDiv x y = Fst (DivMod x y)
+
+-- | The remainder of a type-level integer and a natural number
+--   For a negative number, behaves similarly to 'mod'.
+-- @since 0.1.4
+type family Mod (x :: k) (y :: Nat) :: IntDivK k where
+  Mod x y = Snd (DivMod x y)
+
+-- Fixes up the calculated quotient for a negative dividend.
+-- Subtracts 1 to obtain the behaviour of 'div' instead of 'quot'
 type family DivModNegFixup (x :: (TInt, k)) :: (TInt, k) where
   DivModNegFixup '(a, b) = '( Negate (Add 1 a), b )
+
+-- Converts the result of DivMod for a negative dividend
+-- into the result of QuotRem for a negative dividend
+type family QuotRemFixup (x :: (TInt, TInt)) (y :: Nat) :: (TInt, TInt) where
+  QuotRemFixup '(d,m) y = '(Add 1 d, Sub m y)
+
+-- |
+--
+-- @since 0.1.4
+type family Quot (x :: k) (y :: Nat) :: IntDivK k where
+  Quot x y = Fst (QuotRem x y)
+
+-- |
+--
+-- @since 0.1.4
+type family Rem (x :: k) (y :: Nat) :: IntDivK k where
+  Rem x y = Snd (QuotRem x y)
 
 -- Integer division of positive / positive
 type family DivModAux (x :: TInt) (y :: Nat) (a :: TInt) :: (TInt, TInt) where
   DivModAux ('Pos x) y a = DivModAux (Sub ('Pos x) y) y (Add 1 a)
-  DivModAux ('Neg x) y a = '(Sub a 1, 'Pos x)
+  DivModAux ('Neg x) y a = '(Sub a 1, If (x ==? y) ('Pos 0) ('Pos x))
 
 -- Internal function to unwrap a Pos TInt to a Nat
 type family UnPos (x :: k1) :: k2 where
   UnPos ('Pos x) = x
   UnPos '( 'Pos x, 'Pos y) = '(x, y)
+
+type family Fst (x :: (k1, k2)) :: k1 where
+  Fst '(x,y) = x
+
+type family Snd (x :: (k1, k2)) :: k2 where
+  Snd '(x,y) = y
+
+-- | The absolute value of a type-level number
+--
+-- @since 0.1.4
+type family Abs (x :: k) :: k where
+  Abs (x :: Nat) = x
+  Abs ('Pos x) = 'Pos x
+  Abs ('Neg x) = 'Pos x
+  Abs (x ':% y) = Abs x ':% y
+
+-- | The greatest common divisor of two type-level integers
+--
+-- @since 0.1.4
+type family GCD (x :: k1) (y :: k2) :: GCDK k1 k2 where
+  GCD (x :: Nat) (y :: Nat) = GCDAux x y
+  GCD (x :: Nat) (y :: TInt) = 'Pos (GCDAux x (UnPos (Abs y)))
+  GCD (x :: TInt) (y :: Nat) = 'Pos (GCDAux (UnPos (Abs x)) y)
+
+-- Euclidean algorithm for calculating the GCD of two natural numbers
+type family GCDAux (x :: Nat) (y :: Nat) :: Nat where
+  GCDAux x 0 = x
+  GCDAux x y = GCDAux y (Rem x y)
+
+-- | Reduce a type-level rational into its canonical form
+--
+-- @since 0.1.4
+type family Simplify (x :: Rat) :: Rat where
+  Simplify ((x :: Nat) ':% y) = (IntDiv x (GCD x y)) ':% (IntDiv y (GCD x y))
+  Simplify ((x :: TInt) ':% y) = (IntDiv x (UnPos (GCD x y))) ':% (IntDiv y (UnPos (GCD x y)))
